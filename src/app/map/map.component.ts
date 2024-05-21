6e8dbb533f74dff547a5ab9757232bfac6185d4f
ksoliven <kate.c.soliven@gmail.com>
2024-05-21 08:34:27 -0400

    Added Leaflet library

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,28 @@
+import {AfterViewInit, Component} from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements AfterViewInit {
+
+  // @ts-ignore
+  private map;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+  }
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+}
